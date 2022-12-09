b396512926e50307c76ec75c473114dc67129754
TinNguyen <ductin16111@gmail.com>
2022-12-09 22:36:30 +0200

    second solution for prob 1360 by Python

--- Easy/1360_Number-of-days-between-2-dates.py
+++ Easy/1360_Number-of-days-between-2-dates.py
@@ -1,3 +1,12 @@
+# Write a program to count the number of days between two dates.
+# The two dates are given as strings, their format is YYYY-MM-DD as shown in the examples.
+
+# Input: date1 = "2019-06-29", date2 = "2019-06-30"
+# Output: 1
+
+# Input: date1 = "2020-01-15", date2 = "2019-12-31"
+# Output: 15
+
 month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 def leap(year) -> bool:
     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
@@ -19,7 +28,6 @@ class Solution:
              int(date1[8] + date1[9]))
         d2 = Date(int(date2[0] + date2[1] + date2[2] + date2[3]), int(date2[5] + date2[6]),
                     int(date2[8] + date2[9]))
-
         if d1.year == d2.year:
             result = abs(d1.ordinal() - d2.ordinal())
         else:
@@ -35,4 +43,27 @@ class Solution:
                 result = 365 - before.ordinal() + after.ordinal()
             for i in range(before.year + 1, after.year):
                 result += 365 + leap(i)
-        return result 
+        return result
+    
+class Solution2:
+    def ordinal(date) -> int:
+        y, m, d = map(int, date.split("-"))
+        ordinal = 0
+        if leap(y): month[1] = 29
+        else:       month[1] = 28
+        for i in range(m-1):    ordinal += month[i]
+        ordinal += d
+        return ordinal
+    def days_between_dates(self, date1: str, date2: str) -> int:
+        if date1 > date2:
+            date1, date2 = date2, date1
+        y1, m1, d1 = map(int, date1.split("-"))
+        y2, m2, d2 = map(int, date2.split("-"))
+        d = (date1, date2)
+        ord = map(self.ordinal, d)   
+        ord = list(ord)
+        if y1 == y2:    # same year
+            result = ord[1] - ord[0]
+        else:
+            result = 365 - ord[0] + ord[1] + leap(y1)
+            for i in range(y1 + 1, y2): result += 365 + leap(i)
